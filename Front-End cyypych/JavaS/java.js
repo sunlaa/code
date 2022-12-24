9643bc37d0796e089672bd82532ec4427f46d6cf
sunlaa <santalova.lada25@gmail.com>
2022-12-24 14:12:57 +0300

    updates

--- Front-End cyypych/JavaS/java.js
+++ Front-End cyypych/JavaS/java.js
@@ -1,3 +1,5 @@
+'use strict'
+
 // let imya = prompt('Как тебя зовут?', 'Введи имя');
 // alert(`Тебя зовут ${imya} !`); 
 
@@ -331,7 +333,7 @@
     
 // }
 
-// let accumulator = new Accumulator(2); // начальное значение 1
+// let accumulator = new Accumulator(2); // начальное значение 2
 
 // accumulator.read(); // прибавляет введённое пользователем значение к текущему значению
 // accumulator.read();
@@ -362,19 +364,326 @@
 //  User2.loveCake?.();
 //  alert(User.adress.appartament);
 
-let sym = Symbol('name');
+// let sym = Symbol('name');
 
-let obj = {
-    name: 'Roby',
-    age: '15',
-    [sym]: 'hi',
-}
+// let obj = {
+//     name: 'Roby',
+//     age: '15',
+//     [sym]: 'hi',
+// }
 
-let clone = {};
+// let clone = {};
 
-Object.assign(clone, obj);
+// Object.assign(clone, obj);
 
-alert(clone[sym]);
+// alert(clone[sym]);
 
-alert(sym.description);
+// alert(sym.description);
+
+// let obj1 = {
+//     age: 4,
+//     valueOf() {
+//         return this.age
+//     }
+// }
+
+// alert(obj1 * 4)
+
+
+// let a = +prompt('a?');
+// let b = +prompt('b?');
+
+// if (!isFinite(a)) {
+//     alert('Ожидался ввод чисел')
+// } else if(!isFinite(b)) {
+//     alert('Ожтдался ввод чисел')
+// }else {
+//     alert (a + b)
+// }
+
+
+// function readNumber() {
+//     let num;
+//     do {
+//         num = prompt('Введите число', 0)
+//     } while (!isFinite(num))
+
+//     if (num === null || num === '') return null;
+
+//     return +num;
+// };
+
+// alert(`Число: ${readNumber()}`)
+
+
+// let i = 0;
+// while (i != 10) {
+//  alert( i += 0.2);
+// }
+// for (let i = 0; i != 10; i += 0.2) {
+//     alert(i);
+// }
+
+// function random(min, max) {
+//     return min + Math.random() * (max - min);
+// }
+
+// alert(random(5, 7))
+// alert(random(5, 7))
+// alert(random(5, 7))
+
+// function random(min, max) {
+//     return ((min + Math.random() * (max - min)).toFixed(0));
+
+// }
+
+// alert(random(3, 10))
+// alert(random(3, 10))
+// alert(random(3, 10))
+
+// let str = 'Alla'
+// let str2 = 'Betta'
+
+// alert(str.localeCompare(str2))
+// alert(str < str2)
+
+// function ucFirst(str) {
+//     return (str[0].toUpperCase() + str.slice(1))
+// }
+// alert(ucFirst('lada'))
+// alert(ucFirst('balasrt'))
+// alert(ucFirst('foxFriedPip'))
+
+
+
+// function checkSpam(str) {
+//     str = str.toLowerCase()
+
+//    if (str.includes('viagra') || str.includes('xxx')) {
+//     return true
+//    } else {
+//     return false 
+//    }
+// } 
+
+// alert(checkSpam('buy ViAgRA now'))
+// alert (checkSpam('free xxxxx'))
+// alert ( checkSpam("innocent rabbit"))
+// alert( checkSpam('vIagRa buy'))
+// alert( checkSpam('xXxXXXx'))
+// alert( checkSpam('i love you'))
+
+
+
+// function truncate(str, maxlength) {
+//     if (str.length > maxlength) {
+//         str = (str.slice(0, (maxlength - 3))) + ('...');
+//         return str;
+//     } else {
+//         return str;
+//     }
+// }
+
+// alert( truncate("Вот, что мне хотелось бы сказать на эту тему:", 5))
+
+
+// function extractCurrencyValue(str) {
+//    return +str.slice(1)
+// }
+
+// alert( extractCurrencyValue('$150'))
+
+
+
+// let styles = ['Джаз','Блюз'];
+
+// styles.push('Рок-н-ролл');
+
+// styles[(Math.floor(styles.length/2))] = 'Классика'
+
+// alert(styles.shift())
+
+// styles.unshift('Рэп','Рэгги')
+
+// alert(styles)
+
+
+
+// function camelsize(str) {
+//     return str
+
+//     .split('-')
+
+//     .map( (item, index) => index == 0 ? item : item[0].toUpperCase() + item.slice(1))
+
+//     .join('');
+
+// }
+
+// alert(camelsize('back-ground-color'))
+
+
+// function filterRange(arr, a, b) {
+//     let arrFiltered = arr.filter(item => item >= a && item <= b);
+//     return arrFiltered;
+// }
+
+// let non = [3, 6, 12, 89, 5, 7, 2, 13];
+
+// let newNon = filterRange(non, 5, 15);
+
+// alert(newNon);
+// alert (non);
+
+
+
+
+// function filterRangeInPlace(arr, a, b) {
+//     for (let i = 0; i < arr.length; i++) {
+
+//         if (arr[i] >= b || arr[i] <= a) {
+//             arr.splice(i, 1);
+//               i--; //нужен чтобы сместить элементы по индексу влево при удалении, чтобы не быо пустот и проверка была правильной
+//         }
+        
+//     }
+// }
+
+// let non = [3, 6, 12, 89, 5, 7, 2, 13];
+
+// filterRangeInPlace(non, 5, 15);
+
+// alert(non);
+
+
+
+
+// function sortNum(arr) {
+
+//     arr.sort((a, b) => b - a);
+// }
+
+// let arr = [4, 98, 23, -2, -35, 35, 25];
+
+// sortNum(arr);
+
+// alert( arr );
+
+
+
+
+
+// function copySorted(arr) {
+//     let newCopy = arr.slice();
+//     newCopy.sort();
+//     return newCopy;
+// }
+
+// let arr = ["HTML", "JavaScript", "CSS"];
+
+// let sorted = copySorted(arr);
+
+// alert( sorted ); 
+// alert( arr ); 
+
+
+
+// let vasya = { name: "Вася", age: 25 };
+// let petya = { name: "Петя", age: 30 };
+// let masha = { name: "Маша", age: 28 };
+
+// let users = [ vasya, petya, masha ];
+
+// let names = users.map(item => item.name);
+
+// alert( names ); 
+
+
+
+
+
+// let vasya = { name: "Вася", surname: "Пупкин", id: 1 };
+// let petya = { name: "Петя", surname: "Иванов", id: 2 };
+// let masha = { name: "Маша", surname: "Петрова", id: 3 };
+
+// let users = [ vasya, petya, masha ];
+
+// let usersMapped = users.map( user => (
+//     {fullName: user.name + ' ' + user.surname,
+//      id: user.id,
+
+// }))
+
+// alert( usersMapped[0].id ) // 1
+// alert( usersMapped[0].fullName ) // Вася Пупкин
+
+
+
+
+// function sortByAge(users) {
+//     users.sort((a,b) => a.age - b.age)
+// }
+
+// let vasya = { name: "Вася", age: 25 };
+// let petya = { name: "Петя", age: 30 };
+// let masha = { name: "Маша", age: 28 };
+
+// let arr = [ vasya, petya, masha ];
+
+// sortByAge(arr);
+
+// // теперь: [vasya, masha, petya]
+// alert(arr[0].name); // Вася
+// alert(arr[1].name); // Маша
+// alert(arr[2].name); // Петя
+
+
+// function getAvarageAge(users) {
+//    let ages = users.map(item => item.age)   
+//    let result = Math.round(ages.reduce((sum, item) => sum + item, 0)/users.length) 
+      //решение получше: return Math.round(users.reduce((sumAver, item) => sumAver + item, 0) / user.lenght)
+//    return result
+// }
+
+// let vasya = { name: "Вася", age: 56 };
+// let petya = { name: "Петя", age: 30 };
+// let masha = { name: "Маша", age: 29 };
+
+// let arr = [ vasya, petya, masha ];
+ 
+// alert( getAvarageAge(arr) )
+
+
+
+// function unique(arr) {
+//    return arr.filter((item, i) => i === arr.indexOf(item))
+    
+// }
+
+
+// let strings = ["кришна", "кришна", "харе", "харе",
+//   "харе", "харе", "кришна", "кришна", ":-O"
+// ];
+
+// alert( unique(strings) ); // кришна, харе, :-O
+
+
+
+// function groupById(arr) {
+//    return arr.reduce((obj, item) => {
+//         obj[item.id] = item;
+//         return obj;
+//     }, {})
+// }
+
+// let users = [
+//     {id: 'john', name: "John Smith", age: 20},
+//     {id: 'ann', name: "Ann Smith", age: 24},
+//     {id: 'pete', name: "Pete Peterson", age: 31},
+//   ];
+  
+//   let usersById = groupById(users);
+
+//   alert (usersById)
+
 
